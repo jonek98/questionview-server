4418c928287bf383e11798b356c7783d84fe33e7
jonek98 <93269540+jonek98@users.noreply.github.com>
2024-04-09 23:42:24 +0200

    Add max and min rating value validation

--- src/main/java/com/uni/questionview/domain/entity/RatingEntity.java
+++ src/main/java/com/uni/questionview/domain/entity/RatingEntity.java
@@ -3,6 +3,8 @@ package com.uni.questionview.domain.entity;
 import com.uni.questionview.domain.User;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,6 +23,8 @@ public class RatingEntity {
     @SequenceGenerator(name = "sequenceGenerator")
     private long id;
 
+    @Max(5)
+    @Min(1)
     private int rating;
 
     @ManyToOne
